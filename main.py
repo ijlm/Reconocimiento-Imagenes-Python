7f07affb45651b38f289a509e33a0a0dbf45a501
ivan.laverde.85@gmail.com <Ijlm7021>
2023-05-28 13:19:26 -0500

    primer comit

---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+import time
+import streamlit as st
+
+model = load_model('modelo/modelo.h5') 
+
+def process_image(img, img_width, img_height):
+    img = cv2.resize(img, (img_width, img_height))
+    img = img.reshape(1, img_width, img_height, 3)
+    img = img / 255.0
+    return img
+
+def main():
+    st.title("Reconocimiento Facial")
+    st.write("Presiona el botón 'Iniciar' para comenzar el reconocimiento facial.")
+
+    cap = cv2.VideoCapture(0)
+
+    if st.button("Iniciar"):
+        while True:
+            ret, frame = cap.read()
+            img = process_image(frame, 224, 224)
+            prediction = model.predict(img)
+
+            if prediction < 0.5:
+                label = 'Normal'
+                color = (0, 255, 0)
+            else:
+                label = 'Borracho'
+                color = (0, 0, 255)
+
+            frame = cv2.putText(frame, label + str(prediction), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+            st.image(frame, channels="RGB")
+            
+            if st.button("Detener"):
+                break
+            time.sleep(0.5)
+
+        cap.release()
+
+if __name__ == "__main__":
+    main()
